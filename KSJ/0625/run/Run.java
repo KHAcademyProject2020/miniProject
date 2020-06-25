9a0356199ea1250a2fe611dfd88b28cfc74c5705
soogenie <67138040+soogenie@users.noreply.github.com>
2020-06-25 09:14:02 +0900

    김수진

---
+++ KSJ/0625/run/Run.java
@@ -0,0 +1,9 @@
+package com.kh.project.mini.cafe.view.gui.run;
+
+import com.kh.project.mini.cafe.view.gui.MainFrame;
+
+public class Run {
+	public static void main(String[] args) {
+		new MainFrame();
+	}
+}
